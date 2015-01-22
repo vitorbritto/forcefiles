ba00f969c3f546ae3f8c6aa31bb5647922982cd5
Vitor Britto <code@vitorbritto.com.br>
2015-01-22 10:29:48 -0300

    Add Gone - Shell Script For deployment using Git and FTP

---
+++ scripts/gone.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------------------------------------
+#
+# Program:      gone.sh
+# Author:       Vitor Britto
+# Description:  This script is a shortcut to deployments
+#
+# Usage:        ./gone.sh [options]
+# Example:      ./gone.sh -f
+#
+# Options:
+#       --help, -h           output help instructions
+#       --ftp, -f            deploy to FTP
+#       --git, -g            push to GitHub or Bitbucket
+#
+# Important:
+#     If you prefer, create an alias: gone="bash ~/path/to/script/gone.sh"
+#     And execute with: gone -u http://www.vitorbritto.com.br -r 5
+#
+# Copyright (c) Vitor Britto
+# Licensed under the MIT license.
+# ------------------------------------------------------------------------------------------------------------
